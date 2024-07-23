fd7b2c7ad10f9a6d38e925566bea8496a2be4e5c
Chaprogg <chbbbmo@orange.fr>
2024-07-23 19:03:17 +0200

    scrip.js  slides images access modified and creat right -arrow addEventListener access images

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,29 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.jpg",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
 
-let banner_img = document.querySelector(".banner-img")
+let bannerImg = document.querySelector(".banner-img")
 let arrow_left = document.querySelector(".arrow_left")
 let arrow_right = document.querySelector(".arrow_right")
 let dots = document.querySelector(".dots")
 let activDot = 0
+let activImg = 0
 
 function dotsDisplay () {
 	for (let i =0; i < slides.length; i++) {
@@ -37,4 +38,12 @@ function dotsDisplay () {
 	}
 }
 
+function arrow_rightSelect () {
+	arrow_right.addEventListener("click",() => {
+		activImg++
+		bannerImg.src = slides[activImg].image
+	})
+
+}
 dotsDisplay()
+arrow_rightSelect()
